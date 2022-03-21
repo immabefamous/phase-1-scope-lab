ad0eaa435b60ac822e0ef5730cc2ca22a85d1954
Emmanuel <emmanuelbboatteng@yahoo.com>
2022-03-21 11:48:07 -0400

    commit tree 2522b38de9d4f6a4ab60de3209b664ac5479b91b

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = "bob"
+customerName
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+    return customerName
+}
+var bestCustomer
+function setBestCustomer() {
+   bestCustomer = 'not bob' 
+}
+
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+
+const leastFavortieCharacter = "Jimmy";
+
+function changeLeastFavoriteCustomer() {
+    leastFavortieCharacter = "not jimmy"
+}
